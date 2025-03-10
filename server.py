1fe4ae7719ee8a7d65b2712a47c89e74717e13f0
BigTail <bigtail43118@gmail.com>
2025-03-10 19:26:04 +0900

    update server

--- server.py
+++ server.py
@@ -3,21 +3,55 @@ from fastapi.responses import StreamingResponse
 import base64, io, uvicorn
 from modules.core import process_api
 import os
+import shutil
+import uuid
 
 app = FastAPI()
 
+async def save_input_files(target, source):
+    try:
+        with open(target.filename, 'wb') as f:
+            shutil.copyfileobj(target.file, f)
+    
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        target.file.close()
+
+    try:
+        with open(source.filename, 'wb') as f:
+            shutil.copyfileobj(source.file, f)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        source.file.close()
+
 @app.post("/item")
 async def read_root(
-    source_path: str = Form(...),
-    target_path: str = Form(...),
-    output_path: str = Form(...),
+    target: UploadFile = File(...),
+    source: UploadFile = File(...),
     face_enhancer: bool = Form(...),
 ):
+    print (target.filename)
     if face_enhancer:
         frame_processors = ['face_swapper', 'face_enhancer']
     else:
         frame_processors = ['face_swapper']
+    await save_input_files(target, source)
+    target_path = target.filename
+    source_path = source.filename
+    output = str(uuid.uuid4()) + target_path[-4:]
     PATH = './images'
-    process_api(os.path.join(PATH, source_path), os.path.join(PATH, target_path), os.path.join(PATH, output_path), frame_processors, True, False, False, False, False, False, False, 'libx264', 18, False, True, None, 'cuda', None)
+    process_api(os.path.join(PATH, source_path), os.path.join(PATH, target_path), os.path.join(PATH, output), frame_processors, True, False, False, False, False, False, False, 'libx264', 18, False, True, None, 'cuda', None)
+    img = open(os.path.join(PATH, output), 'rb')
 
-    return {"Hello": "World"}
+    if output.endswith('.jpg'):
+        media_type = "image/jpeg"
+    elif output.endswith('.mp4'):
+        media_type = "video/mp4"
+    elif output.endswith('.gif'):
+        media_type = "image/gif"
+    else:
+        raise ValueError("Unsupported file format")
+    
+    return StreamingResponse(io.BytesIO(img.read()), media_type=media_type)
